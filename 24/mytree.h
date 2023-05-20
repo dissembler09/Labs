aeda5628acd45c62954136914e19893ac38347b3
dissembler09 <127303040+dissembler09@users.noreply.github.com>
2023-05-20 22:20:13 +0300

    Add files via upload

---
+++ 24/mytree.h
@@ -0,0 +1,48 @@
+#ifndef MYTREE
+#define MYTREE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef enum _type {
+    OPERATOR,
+    NUMBER,
+    SIGN,
+    VARIABLE
+}Type;
+
+typedef struct _node {
+    Type type;
+    int val, rang;
+    /*val - значение узла - число или код для знака;
+    rang - приоритет операции;*/
+    char ch;
+    struct _node *l, *r;//указатели на правый и левый дочерние узлы
+}Node;
+
+typedef struct _stack{
+    Node* head;
+}stack;
+
+typedef struct _tree{
+    Node* root;
+}tree;
+
+tree new_tree();
+stack new_stack();
+Node* create_node(Type type, int val, int rang);
+void push(stack* now, Node* a);
+Node* delet(stack* n);
+void stack_push(stack* st, stack* res, Node* now);
+Node* add (Node* now, stack* s);
+void build(tree* t, stack* s);
+void decoding(Node * n);
+void print_tree(Node* root, int n);
+void print_node(Node* n, int l, int r);
+Node* copy (Node* a);
+bool solving(Node* n);
+void transform(tree* t);
+void print_infix(tree* t);
+void free_node(Node* n);
+void free_tree(tree* tr);
+#endif
