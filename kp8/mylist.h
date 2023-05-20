1a95d114347daf454ec8df573f1989192576f236
dissembler09 <127303040+dissembler09@users.noreply.github.com>
2023-05-20 21:19:54 +0300

    Add files via upload

---
+++ kp8/mylist.h
@@ -0,0 +1,25 @@
+#ifndef MYLIST
+#define MYLIST
+#include <stdlib.h>
+
+typedef struct Node {
+    double value;
+    struct Node *next;
+} Node; //обозначим структуру на которой будет базироваться список
+
+typedef struct List {
+    Node* bar;
+    int size;
+} List;//структура самого списка
+
+int sizeList(List* list);
+Node* createNode(double data);
+List* createList();
+List* reversed_list(List* list);
+void insertList(List* list, double data);
+void removeNode(List* list, double data);
+void printlist(List *list);
+void clearList(List* list);
+void removeLastKNodes(List* list, int k);
+double get_element(List* list, int index);
+#endif
